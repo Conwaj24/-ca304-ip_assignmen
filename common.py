f6d41c9673e6cb3b64fd9a2ae6c1567e5cbc865c
JCoMcL <rapcyss@gmail>
2020-10-25 18:43:58 +0000

    add common.py for generic helpers, add list_add_and_str function to simplify a common list comprehension

---
+++ common.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+def list_add_and_str(list, i=0):
+    '''for each member of the given list, add i and convert to string'''
+    return [str(e + i) for e in list]
+
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
